687e20c7efe63a320c73af2c4af8a489161683a6
松谷 健太朗 <matsutani@geisia.jp>
2019-11-15 15:02:28 +0900

    thread page

--- src/pages/dashboard/thread.tsx
+++ src/pages/dashboard/thread.tsx
@@ -3,12 +3,9 @@ import Link from "next/link";
 
 const Page: NextPage = () => (
   <h1>
-    index page!
+    thread page
     <div>
-      <Link href="/contact">to contact</Link>
-    </div>
-    <div>
-      <Link href="/dashboard/thread">to thread</Link>
+      <Link href="/">to top</Link>
     </div>
   </h1>
 );
